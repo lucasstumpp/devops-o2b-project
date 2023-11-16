0709e61595588259402fb18a5542c8c278a8e830
magamen <daniel_totty@live.com>
2023-11-16 17:11:22 -0300

    Adicionando diretorio Organization e seus respectivos arquivos terraform e adicionado output.tf no diretorio ec2

---
+++ modules/organization/variables.tf
@@ -0,0 +1,7 @@
+variable "tcb_blog_vpc_id" {
+  default = "" # Orientações para copia da VPC ID abaixo.
+}
+
+variable "tcb_blog_subnet_public_id" {
+  default = "" # Orientações para copia da Subnet ID abaixo.
+}
