b778011c090107c6569b9f721f64a332dddf9b05
magamen <daniel_totty@live.com>
2023-11-17 17:42:20 -0300

    adicionando resource para organization

--- modules/organization/resource.tf
+++ modules/organization/resource.tf
@@ -1,23 +1,8 @@
-data "aws_ami" "ubuntu" {
-  most_recent = true
+resource "aws_organizations_organization" "org" {
+  aws_service_access_principals = [
+    "cloudtrail.amazonaws.com",
+    "config.amazonaws.com",
+  ]
 
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  owners = ["099720109477"] # Ubuntu
-}
-
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
-  subnet_id = var.tcb_blog_subnet_public_id
-  vpc_security_group_ids = [aws_security_group.permitir_ssh_http.id]
-  associate_public_ip_address = true
-  
-    tags = {
-    Name = "blogserver01"
- # Insira o nome da instância de sua preferência.
-  }
+  feature_set = "ALL"
 }
